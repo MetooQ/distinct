89b0ac5a0499eba6a70e52c627468ed603ca3dcd
maqian <maqian1220@gmail.com>
2015-04-04 12:24:29 +0800

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# distinct
-distinct array
--- 快盘/work/github/toRGB.js
+++
@@ -1,5 +0,0 @@
-function toRGB(color) {
-    var regex = /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/
-    match = color.match(regex)
-    return match ? 'rgb('+parseInt(match[1], 16)+','+parseInt(match[2], 16)+','+parseInt(match[3], 16)+')' : color
-}
